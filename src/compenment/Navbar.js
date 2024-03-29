d46810cbb5c82751bddcfe650223e1eb7e303d58
Anushan <anushan559@gmail.com>
2024-03-29 13:41:37 +0530

    complete landing page

---
+++ src/compenment/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Button from "./Button"
+import { GiFoodTruck } from "react-icons/gi";
+import  "../Stylecss/Navbar.css"
+import background from "../img/cooking-food.jpg"
+import { auto } from "async";
+const Navbar=()=>{
+    return(
+        <div>
+            <body style={{ backgroundImage: `url(${background})` ,backgroundRepeat: 'no-repeat' , height:"100vh",width:"100%",backgroundSize: "cover"
+                        ,backgroundPosition:"center",position:"relative"}}>
+                <div className="container" >
+                    <div className="nav">
+                        <GiFoodTruck className="logo"/>
+
+                        <ul>
+                            <li><a href="HOME">HOME</a></li>
+                            <li><a href="MENU">MENU</a></li>
+                            <li><a href="ABOUT">ABOUT</a></li>
+                            <li><a href="SHOP">SHOP</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <div className="content">
+                            <p className="quotes">GOOD FOOD GOOD HEALTH</p>
+                        </div>
+                        
+                    </div>
+                       <Button title="ORDER NOW / RESERVATION"/>
+                       
+                </div>
+                        
+            </body>
+        </div>
+    )
+}
+
+export default Navbar
